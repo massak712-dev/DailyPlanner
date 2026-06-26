3e732de27dca99e8853224e6131e4ca74402796b
sapfear <massak712@gmail.com>
2026-06-26 13:10:11 +0300

    добавлено: ввод задач и вывод задач, также работа со временем, отображение сколько осталось до конца дедлайна.

--- DailyPlanner/Program.cs
+++ DailyPlanner/Program.cs
@@ -33,11 +33,11 @@ namespace DailyPlanner
 
                     if (choice == "1")
                     {
-                        //AddTask();
+                        AddTask();
                     }
                     else if (choice == "2")
                     {
-                        //ShowTasks();
+                        showTasks();
                     }
                     else if (choice == "3")
                     {
@@ -63,6 +63,94 @@ namespace DailyPlanner
                     Console.ReadKey();
                 }
             }
-
+        static void AddTask()
+        {
+            Console.Clear();
+            Console.WriteLine("Добавлние новой задачи");
+            Console.Write("Введите задачу: ");
+            string task=Console.ReadLine();
+            if(string.IsNullOrEmpty(task))
+            {
+                Console.WriteLine("задача не может быть пустой");
+                return;
+            }
+            Console.WriteLine("\n Введите дедлайн:");
+            Console.WriteLine(" дд.мм.гггг чч:мм  (например: 28.06.2026 18:00)");
+            string deadLineInput=Console.ReadLine();
+            DateTime deadLine= DateTime.Parse(deadLineInput);
+            tasks.Add(task);
+            deadlines.Add(deadLine);
+            //сохранение
+            Console.WriteLine("задачу добавлена");
         }
+        static void showTasks()
+        {
+            if(tasks.Count==0)
+            {
+                Console.WriteLine("нет заданий");
+                return;
+            }
+            for(int i=0;i<tasks.Count;i++)
+            {
+                Console.Write($"{i + 1}. {tasks[i]}");
+                TimeSpan remaining = deadlines[i]-DateTime.Now; ;
+                double remainingHours=remaining.TotalHours;
+                double totalTime = (deadlines[i] - DateTime.Now).TotalHours;
+                if (remaining.TotalHours < 0)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.Write($" ПРОСРОЧЕНО! (-{(-remaining).Days}д {(-remaining).Hours}ч)");
+                    Console.ResetColor();
+                }
+                else
+                {
+                    double totalHourse = (deadlines[i] - DateTime.Now).TotalHours;
+                    if (deadlines[i] == DateTime.Now.Date)
+                    {
+                        double percent = (remaining.TotalHours / 24) * 100;
+                        if (percent > 70)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if (percent > 20)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        Console.Write($"{remaining:hh\\:mm} (осталось)");
+                        Console.ResetColor();
+                    }
+                    else
+                    {
+                        int daysLeft = remaining.Days;
+                        int HourseLeft = remaining.Hours;
+                        if (daysLeft > 3)
+                        {
+                            Console.ForegroundColor = ConsoleColor.White;
+                        }
+                        else if (daysLeft > 1)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Yellow;
+                        }
+                        else
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                        if (daysLeft > 0)
+                            Console.Write($"{daysLeft}д {HourseLeft}ч (осталось)");
+                        else
+                            Console.Write($"{HourseLeft}ч (осталось)");
+
+                        Console.ResetColor();
+                    }
+
+                }
+            }
+        }
+
+    }
+    
     }
